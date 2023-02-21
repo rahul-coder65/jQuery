17005f1bb78ef0412fbe07b4fbcdc05a9192ba7d
Rahul Kumar Singh <rahulsingh600500@gmail.com>
2023-02-21 12:04:41 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// $("h1").css("color","red");
+// $("h1").addClass("new-btn");
+// $("button").text("don't click me");
+// $("button").html("<b>Hey Rahul</b>")
+// $("a").attr("href","https://www.youtube.com");
+// $("a").text("You Tube");
+// console.log($("h1").attr("class"));
+// $("h1").click(function(){
+    // $("h1").css("color","purple");
+// });
+
+// for(var i=0;i<5;i++){
+//     document.querySelectorAll("button")[i].addEventListener("click",function(){
+//         document.querySelector("h1").style.color = "purple";
+
+//     });
+// }
+
+// $("button").click(function(){
+//    $("h1").css("color","green"); 
+// })
+
+// $(document).keypress(function(event){
+//     $("h1").text(event.key);
+// });
+// $("h1").on("mouseover",function(){
+    // $("h1").css("color","pink");
+// });
+
+
+// $("h1").before("<button>New</button>");
+// $("h1").after("<button>New</button>");
+// $("h1").prepend("<button>New</button>");
+// $("h1").append("<button>New</button>");
+
